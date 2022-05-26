f37ebb6bc9d850fe952f19743285fea8a791e4f7
Joshua Gleaton <jgleaton@gowestrobotics.com>
2022-05-26 11:47:21 -0400

    add grasp test

---
+++ python/psyonic-ability-hand/src/psyonic_ability_hand/hand-grasp-test.py
@@ -0,0 +1,53 @@
+import time
+from psyonic_ability_hand import log
+from psyonic_ability_hand.hand import Hand, MockComm
+from psyonic_ability_hand.io import SerialIO
+
+
+def main():
+    log.info("starting grasp test")
+
+#    hand = Hand(MockComm())
+    hand = Hand(SerialIO())
+
+    try:
+        hand.start()
+
+        hand.grasp(width=0, speed=.1)
+        time.sleep(10)
+
+        hand.grasp(width=1, speed=.1)
+        time.sleep(10)
+
+        hand.grasp(width=0, speed=.4)
+        time.sleep(6)
+
+        hand.grasp(width=.5, speed=.4)
+        time.sleep(6)
+
+        hand.grasp(width=0, speed=.3)
+        time.sleep(6)
+
+        hand.grasp(width=1, speed=.3)
+        time.sleep(6)
+
+        hand.grasp(width=0, speed=6)
+        time.sleep(10)
+
+
+        # t = 0
+        # while True:
+        #     t += 1
+        #     time.sleep(1)
+        #     print(hand.stats())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        hand.stop()
+
+    print("Hand stopped")
+    print(hand.stats())
+
+
+if __name__ == '__main__':
+    main()
